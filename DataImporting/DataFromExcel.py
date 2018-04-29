caf81d98fe4678a8b6c4dfa50fc601ec9b075415
noga <nogur9@gmail.com>
2018-04-29 18:23:00 +0300

    "fuck it all"

--- DataImporting/DataFromExcel.py
+++ DataImporting/DataFromExcel.py
@@ -10,7 +10,7 @@ def get_data(path, sheet_name):
     return dataset
 
 
-def create_new_excel (path, sheet_name):
+def create_new_excel(path, sheet_name):
     df = get_data(path, sheet_name)
     previuos_trial = ""
     for index, row in df.iterrows():
@@ -28,9 +28,9 @@ def create_new_excel (path, sheet_name):
 
 def print_data(dataset, group_column_name):
 
-    print (dataset.head(20))
-    print (dataset.describe())
-    print (dataset.shape)
+    #print(dataset.head(20))
+    print(dataset.describe())
+    #print(dataset.shape)
     print(dataset.groupby(group_column_name).size())
 
 
@@ -41,6 +41,20 @@ def plot_data(dataset):
 
     scatter_matrix(dataset)
     plt.show()
+
+
+def box_plot(dataset):
+    """
+
+    :param dataset: Pandas DataFrame dataset
+
+    Makes box and whisker plots without the outcome variable.
+    """
+
+    dataset_without_output = dataset.drop('group', axis=1)
+    dataset_without_output.plot(kind='box', subplots=True, layout=(2, 4), sharex=False, sharey=False,Columns = ['Age','PHQ9'])
+    plt.show()
+    plt.close()
 
 
 def refactor_labels(dataset, group_column_name):
@@ -67,8 +81,8 @@ def plot_scatters(dataset):
     zero_vals = [dataset.values[i] for i in range(len(dataset.values)) if dataset.values[i][0] == 0.0]
     one_values = [dataset.values[i] for i in range(len(dataset.values)) if dataset.values[i][0] == 1.0]
 
-    for i in range(2, 19):
-        for j in range(2,i):
+    for i in range(1):
+        for j in range(29,34):
             red_patch = mpatches.Patch(color='red', label='Non SAD')
             blue_patch = mpatches.Patch(color='blue', label='SAD')
             plt.legend(handles=[red_patch, blue_patch])
@@ -85,10 +99,12 @@ def plot_scatters(dataset):
 
 
 
-#dataset = refactor_labels(get_data("C:\\Users\\user\\PycharmProjects\\AnxietyClassifier\\ExtractedFeatures\\formatted_features.xlsx", "Sheet1"),"group")
+#dataset = refactor_labels(get_data("C:\\Users\\user\\PycharmProjects\\AnxietyClassifier(2)\\DataImporting\\Features_OverallFixationData_no_outlayers.xlsx", "Sheet1"),"group")
 #plot_scatters(dataset)
 #create_new_excel('C:\\Users\\user\\PycharmProjects\\AnxietyClassifier\\AmitsData\\SAD.xlsx','fixation results')
 #count_missing_data(dataset)
 #print_data(dataset, 'group')
 #refactor_labels(dataset, 'group')
-#plot_data(dataset)
+#plot_scatters(dataset)
+#print_data(dataset,"group")
+#box_plot(dataset)
